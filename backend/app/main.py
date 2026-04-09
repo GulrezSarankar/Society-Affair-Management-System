e4cbe4c1155d5ed045656a3d8af70f2caa97a89c
gulrez121 <gulrezsarankar16@gmail.com>
2026-04-09 12:53:07 +0530

    comit

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,33 +2,42 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from app.auth import router as auth_router
 from app.flat_routes import router as flat_router
-from fastapi.middleware.cors import CORSMiddleware  
+from fastapi.middleware.cors import CORSMiddleware
 from app.maintenance import router as maintenance_router
 from app.Resident import router as resident_router
 from fastapi.staticfiles import StaticFiles
 from app.Watchman import router as watchman_router
+import os
 
+# Create uploads folder if not exists
+if not os.path.exists("uploads"):
+    os.makedirs("uploads")
+
+# Create tables
 Base.metadata.create_all(bind=engine)
-
 
 app = FastAPI()
 
+# CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # for development
+    allow_origins=["*"],  # change later to frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    
 )
 
+# Routes
 app.include_router(auth_router)
 app.include_router(watchman_router)
 app.include_router(flat_router)
-app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
-
 app.include_router(resident_router)
 app.include_router(maintenance_router)
+
+# Static files
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
+
+# Test route
 @app.get("/")
 def home():
-    return {"message": "Society Management System API running"}
+    return {"message": "Society Management System API running 🚀"}
